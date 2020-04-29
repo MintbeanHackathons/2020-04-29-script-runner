32b6668291036545d3e3a3a5555ceef16c411810
Derek Johnston <Derek@dj-design.ca>
2020-04-29 15:14:04 -0400

    should be blocking pages with "Derek" in header...

--- public/background.js
+++ public/background.js
@@ -1,3 +1,14 @@
-chrome.browserAction.onClicked.addListener(function() {
-  chrome.tabs.create({ url: chrome.extension.getURL('index.html'), selected: true });
-});
+chrome.webRequest.onBeforeSendHeaders.addListener(
+  function(details) {
+    for (var i = 0; i < details.requestHeaders.length; ++i) {
+      if (details.requestHeaders[i].name === "Derek") {
+        details.requestHeaders.splice(i, 1);
+        alert("blocked")
+        break;
+      }
+    }
+    return { requestHeaders: details.requestHeaders };
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking", "requestHeaders"]
+);
---
+++ src/components/PageBlocker.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+  .container
+    .row
+      .col
+        b-form-textarea(
+          id="textarea"
+          v-model="text"
+          placeholder="Enter something..."
+          rows="3"
+          max-rows="6")
+    .row
+      .col
+        b-button(@click="replace") Replaces
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: ""
+    }
+  },
+  methods: {
+    replace() {
+      document.body.innerHTML = this.text
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
